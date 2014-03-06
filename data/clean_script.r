9a9f26987aef69b4d3a2b4c521fcd771b190dd04
Matt Parker <mattmparker@gmail.com>
2014-03-06 15:26:12 -0700

    Add some descriptive errors for data path problems.

--- data/clean_script.r
+++ data/clean_script.r
@@ -7,6 +7,21 @@ options(stringsAsFactors = FALSE)
 library(to1check)
 
 
+
+# Try to set the working directory
+if(file.exists(file.path("..", "data"))) {
+    setwd("../data") } else {
+        stop("Make sure your working directory is set correctly (type getwd() into the console to see its current setting).") }
+
+
+# Make sure dataextract.zip is in there
+if(file.exists(file.path("originals", "dataextract.zip"))) {
+    extractpath <- file.path("originals", "dataextract.zip") } else {
+        stop("dataextract.zip doesn't seem to be in data/originals. Did you download it? Is it named exactly 'dataextract.zip'?") }
+
+
+
+
 # Clean the TO 1 data and output a nice, tidy list
 to1clean <- clean_to1(extractfile = file.path("originals", "dataextract.zip"))
 
